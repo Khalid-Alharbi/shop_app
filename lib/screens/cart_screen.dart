13e35a50840662d6eae373e3d161573aae656ae2
Khalid-Alharbi <kjharbi@hotmail.com>
2022-04-29 21:43:13 +0300

    attaching token to post request and using proxy provider

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -57,7 +57,6 @@ class CartScreen extends StatelessWidget {
                 // cart.items[i]!.id,
                 cart.items.values.toList()[i].id,
                 cart.items.keys.toList()[i],
-
                 cart.items.values.toList()[i].price,
                 cart.items.values.toList()[i].quantity,
                 cart.items.values.toList()[i].title,
